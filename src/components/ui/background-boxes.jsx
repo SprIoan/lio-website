771d52d54061c5e2d7c63dbbee8fdbeecab6c578
Spiros Ioannou <sioannou@grnet.gr>
2024-10-26 12:53:25 +0300

    Compromise Design for Performance

--- src/components/ui/background-boxes.jsx
+++ src/components/ui/background-boxes.jsx
@@ -5,8 +5,8 @@ import { cn } from "../../lib/utils";
 
 //eslint-disable-next-line
 export const BoxesCore = ({ className, ...rest }) => {
-  const rows = new Array(40).fill(1);
-  const cols = new Array(36).fill(1);
+  const rows = new Array(30).fill(1);
+  const cols = new Array(30).fill(1);
   let colors = [
     "",
     "--sky-400",
@@ -44,18 +44,18 @@ export const BoxesCore = ({ className, ...rest }) => {
   const screenSize = window.innerWidth;
   let screen_limit = 22;
   if (screenSize < 450) {
-    screen_limit = 14;
+    screen_limit = 5;
   }
   if (screenSize < 768) {
-    screen_limit = 17;
+    screen_limit = 5;
   } else if (screenSize < 1441) {
-    screen_limit = 19;
+    screen_limit = 5;
   } else {
-    screen_limit = 21;
+    screen_limit = 5;
   }
 
   const getRandomColor = (i, j) => {
-    if (j < screen_limit || Math.abs(i - j) < 5) {
+    if (j < screen_limit || Math.abs(i - j) < 3) {
       return colors2[Math.floor(Math.random() * colors2.length)];
     }
     return colors[Math.floor(Math.random() * colors.length)];
@@ -64,7 +64,7 @@ export const BoxesCore = ({ className, ...rest }) => {
   return (
     <div
       style={{
-        transform: `translate(-25%,-20%) skewX(-48deg) skewY(14deg) scale(1.2) rotate(0deg) translateZ(0)`,
+        transform: `translate(-25%,25%) scale(1.1) rotate(0deg) translateZ(0)`,
       }}
       className={cn(
         "absolute left-1/4 p-4 -top-1/4 flex  -translate-x-1/2 -translate-y-1/2 w-full h-full z-0 ",
