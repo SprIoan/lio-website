94be3ce681e9ede23ecdb1c02bb7e5c396c73071
Spiros Ioannou <sioannou@grnet.gr>
2024-10-26 17:16:31 +0300

    Finish Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,28 @@ import { Boxes } from "../components/ui/background-boxes";
 import { cn } from "../lib/utils";
 import Loading from "../components/ui/Loading";
 import { useTheme } from "../ThemeContext";
+import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
 
+const AceternityLogo = () => {
+  return (
+    <svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-3 w-3 text-black dark:text-white"
+    >
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
 const Hero = () => {
   const { theme } = useTheme();
   return (
@@ -22,7 +43,7 @@ const Hero = () => {
         <Boxes />
         <h1
           className={cn(
-            `md:text-6xl text-3xl font-semibold z-0 pointer-events-none ${
+            `md:text-6xl lg:text-7xl text-3xl font-semibold z-0 pointer-events-none border-4 border-black dark:border-white shadow-2xl p-4 bg-white dark:bg-black ${
               theme == "light" ? "text-black" : "text-white"
             } relative z-20`
           )}
@@ -30,12 +51,24 @@ const Hero = () => {
           Welcome to Lio Tech
         </h1>
         <p
-          className={`text-center text-lg md:text-2xl mt-2 pointer-events-none ${
-            theme == "light" ? "text-gray-600" : "text-gray-300"
+          className={`text-center text-lg md:text-2xl font-bold  underline-offset-4 lg:text-4xl mt-6 pointer-events-none ${
+            theme == "light" ? "text-black" : "text-white"
           } relative z-20`}
         >
           where we build amazing things for you
         </p>
+        <div className="relative top-[150px] md:top-10 flex justify-center text-center">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            <AceternityLogo />
+            <span className="whitespace-nowrap text-lg md:text-xl">
+              Learn More
+            </span>
+          </HoverBorderGradient>
+        </div>
       </div>
     </Suspense>
   );
