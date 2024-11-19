41fe0a8a3a7edf6869a7dfd0b651d96c96e26577
Spiros Ioannou <sioannou@grnet.gr>
2024-11-19 15:39:43 +0200

    Contact Light Mode

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -18,24 +18,33 @@ const Contact = () => {
 
     if (copyBtnNo === 1) {
       navigator.clipboard.writeText("test@test.com");
-      const copyIcon = document.querySelector(".copyIcon1");
+      const copyIcon = document.querySelector(".copyIcon1a");
       copyIcon.src = "/images/checkmark.svg";
+      const copyIcon2 = document.querySelector(".copyIcon1b");
+      copyIcon2.src = "/images/checkmark.svg";
       setTimeout(() => {
-        copyIcon.src = "/images/copy.svg";
+        copyIcon.src = "/images/copy-dark.svg";
+        copyIcon2.src = "/images/copy-light.svg";
       }, 3000);
     } else if (copyBtnNo === 2) {
       navigator.clipboard.writeText("Michail Voda 148, 10446, Athens, Greece");
-      const copyIcon = document.querySelector(".copyIcon2");
+      const copyIcon = document.querySelector(".copyIcon2a");
       copyIcon.src = "/images/checkmark.svg";
+      const copyIcon2 = document.querySelector(".copyIcon2b");
+      copyIcon2.src = "/images/checkmark.svg";
       setTimeout(() => {
-        copyIcon.src = "/images/copy.svg";
+        copyIcon.src = "/images/copy-dark.svg";
+        copyIcon2.src = "/images/copy-light.svg";
       }, 3000);
     } else {
       navigator.clipboard.writeText("+30 123 456 789 0");
-      const copyIcon = document.querySelector(".copyIcon3");
+      const copyIcon = document.querySelector(".copyIcon3a");
       copyIcon.src = "/images/checkmark.svg";
+      const copyIcon2 = document.querySelector(".copyIcon3b");
+      copyIcon2.src = "/images/checkmark.svg";
       setTimeout(() => {
-        copyIcon.src = "/images/copy.svg";
+        copyIcon.src = "/images/copy-dark.svg";
+        copyIcon2.src = "/images/copy-light.svg";
       }, 3000);
     }
   };
@@ -48,43 +57,51 @@ const Contact = () => {
       <div className="container border-2 dark:border-0 border-gray-800 lg:rounded-[50px] py-3 dark:lg:bg-gray-900/70 flex flex-col lg:flex-row lg:gap-6">
         <div className="w-full flex flex-col items-center lg:items-start lg:w-2/5 h-full rounded-3xl ">
           <img
-            src="/images/test2.svg"
+            src="/images/contact-dark.svg"
             alt="lio-dark"
             className="w-1/2 ml-10 lg:ml-3 mt-2 hidden dark:flex"
           />
           <img
-            src="/images/lio-light.svg"
+            src="/images/contact-light.svg"
             alt="lio-light"
             className="w-1/2 ml-10 lg:ml-3 mt-2 flex dark:hidden"
           />
-          <div className="relative mt-16 flex flex-col justify-center group px-6 py-4 border border-black dark:border-0 rounded-3xl w-[90%] bg-[#A20A12] dark:bg-black/15 shadow-sm  dark:shadow-purple-200 hover:shadow-md dark:hover:bg-black/25 transition-colors">
+          <div className="relative mt-16 flex flex-col justify-center group px-6 py-4 rounded-3xl w-[90%] bg-white/75 dark:bg-black/15 shadow-sm  hover:bg-white dark:hover:bg-black/40 transition-colors">
             <a href="mailto:test@test.com">
               <img
                 src="/images/chat.svg"
                 alt="chat"
-                className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#2BBF63] dark:bg-[#613594] group-hover:bg-[#4edc85] dark:group-hover:bg-[#904edb] transition-colors"
+                className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#46e482] dark:bg-[#613594] group-hover:bg-[#30dc72] dark:group-hover:bg-[#904edb] transition-colors"
               />
             </a>
-            <h1 className="text-lg font-semibold  text-white ">Chat to us</h1>
-            <p className="text-xs mt-3 text-gray-400">
+            <h1 className="text-lg font-semibold text-black dark:text-white  ">
+              Chat to us
+            </h1>
+            <p className="text-xs mt-3 text-gray-600 dark:text-gray-400">
               Our friendly team is here to help
             </p>
-            <p className="text-xs mt-5 font-semibold text-gray-300">
+            <p className="text-xs mt-5 font-semibold text-gray-500 dark:text-gray-300">
               test<span className="hidden">NoSpam</span>@
               <span className="hidden">NoSpam</span>test.com
             </p>
             <img
-              src="/images/copy.svg"
+              src="/images/copy-dark.svg"
               alt="copy"
-              className="copyIcon1 absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              className="hidden dark:flex copyIcon1a absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              onClick={() => handleCopy(1)}
+            />
+            <img
+              src="/images/copy-light.svg"
+              alt="copy"
+              className="flex dark:hidden copyIcon1b absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
               onClick={() => handleCopy(1)}
             />
           </div>
-          <div className="mt-6 relative flex flex-col justify-center group px-6 py-4  rounded-3xl w-[90%] bg-black/15 shadow-sm shadow-purple-200 hover:bg-black/25 transition-colors">
+          <div className="mt-6 relative flex flex-col justify-center group px-6 py-4  rounded-3xl w-[90%] bg-white/75 dark:bg-black/15 shadow-sm hover:bg-white dark:hover:bg-black/40 transition-colors">
             <img
               src="/images/location.svg"
               alt="chat"
-              className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#56ec90] dark:bg-[#613594] group-hover:bg-[#2BBF63] dark:group-hover:bg-[#904edb]  transition-colors cursor-pointer"
+              className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#46e482] dark:bg-[#613594] group-hover:bg-[#56ec90] dark:group-hover:bg-[#904edb]  transition-colors cursor-pointer"
               onClick={handleLocationClick}
             />
             <h1 className="text-lg font-semibold text-black dark:text-white ">
@@ -93,22 +110,28 @@ const Contact = () => {
             <p className="text-xs mt-3 text-gray-600 dark:text-gray-400">
               Come say hello at our office HQ.
             </p>
-            <p className="text-xs mt-5 font-semibold text-gray-300">
+            <p className="text-xs mt-5 font-semibold text-gray-500 dark:text-gray-300">
               Michail Voda 148 <br /> 10446, Athens, Greece
             </p>
             <img
-              src="/images/copy.svg"
+              src="/images/copy-dark.svg"
               alt="copy"
-              className="copyIcon2 absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              className="hidden dark:flex copyIcon2a absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              onClick={() => handleCopy(2)}
+            />
+            <img
+              src="/images/copy-light.svg"
+              alt="copy"
+              className="flex dark:hidden copyIcon2b absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
               onClick={() => handleCopy(2)}
             />
           </div>
-          <div className="mt-6 relative flex flex-col justify-center group px-6 py-4 rounded-3xl w-[90%] bg-black/15 shadow-sm shadow-purple-200 hover:bg-black/25 transition-colors">
+          <div className="mt-6 relative flex flex-col justify-center group px-6 py-4  rounded-3xl w-[90%] bg-white/75 dark:bg-black/15 shadow-sm hover:bg-white dark:hover:bg-black/40 transition-colors">
             <a href="tel:+301234567890">
               <img
                 src="/images/phone.svg"
                 alt="chat"
-                className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#56ec90] dark:bg-[#613594] group-hover:bg-[#2BBF63] dark:group-hover:bg-[#904edb]  transition-colors"
+                className="w-8 h-8 p-2  rounded-xl absolute top-3 right-4 bg-[#46e482] dark:bg-[#613594] group-hover:bg-[#2BBF63] dark:group-hover:bg-[#904edb]  transition-colors"
               />
             </a>
             <h1 className="text-lg font-semibold text-black dark:text-white">
@@ -117,22 +140,28 @@ const Contact = () => {
             <p className="text-xs mt-3 text-gray-600 dark:text-gray-400">
               Mon-Fri from 9am to 5pm
             </p>
-            <p className="text-xs mt-5 font-semibold text-gray-300">
+            <p className="text-xs mt-5 font-semibold text-gray-500 dark:text-gray-300">
               <p>
                 {" "}
                 +30 <span className="hidden">NoSpam</span>123 456 789 0
               </p>{" "}
             </p>
             <img
-              src="/images/copy.svg"
+              src="/images/copy-dark.svg"
               alt="copy"
-              className="copyIcon3 absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              className="hidden dark:flex copyIcon3a absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
+              onClick={() => handleCopy(3)}
+            />
+            <img
+              src="/images/copy-light.svg"
+              alt="copy"
+              className="flex dark:hidden copyIcon3b absolute right-6 bottom-4 cursor-pointer hover:scale-110 transition-transform"
               onClick={() => handleCopy(3)}
             />
           </div>
         </div>
 
-        <div className="w-fit relative right-0 xl:-right-12 border bg-[#2BBF63] dark:bg-purple-500/50 h-full rounded-[50px] p-10 py-16 mt-10 lg:mt-0">
+        <div className="w-fit relative right-0 xl:-right-12 border bg-[#30dc72] dark:bg-purple-500/50 h-full rounded-[50px] p-10 py-16 mt-10 lg:mt-0">
           <h1 className="font-light text-5xl text-black dark:text-gray-200">
             Got Ideas? We&apos;ve got the skills. Let&apos;s team up.
           </h1>
