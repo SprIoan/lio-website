452feb1ac4adeac23a7029af02f1194699dfeac6
Spiros Ioannou <sioannou@grnet.gr>
2025-11-12 15:53:42 +0200

    Iso Logo

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import useSound from "use-sound";
 
 import note1 from "../assets/sounds/note1.mp3";
@@ -20,25 +22,56 @@ const Footer = () => {
   const getYear = () => {
     return new Date().getFullYear();
   };
+
   return (
     <section
       id="footer"
-      className="bg-gradient-to-b from-[#EAEBED] to-white dark:from-[#040711] dark:to-black  h-auto pb-4"
+      className="bg-gradient-to-b from-[#EAEBED] to-white dark:from-[#040711] dark:to-black h-auto pb-4"
     >
-      <div className=" container py-8">
-        <div className="flex justify-between items-center">
-          <p className="text-sm text-black dark:text-white/90">
+      <div className="container py-8">
+        {/* Bottom section with copyright and interactive notes */}
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-6">
+          <p className="text-sm text-black dark:text-white/90 order-2 md:order-1">
             &copy; {getYear()} Lio Tech. All rights reserved.
           </p>
-          <div className="flex gap-0.5">
-            <div className="w-3 h-4 bg-[#33DC72]" onClick={playNote1}></div>
-            <div className="w-3 h-4 bg-[#DEB887]" onClick={playNote2}></div>
-            <div className="w-3 h-4 bg-[#F98B8B]" onClick={playNote3}></div>
-            <div className="w-3 h-4 bg-[#9EA7FA]" onClick={playNote4}></div>
-            <div className="w-3 h-4 bg-[#6BCFFA]" onClick={playNote5}></div>
-            <div className="w-3 h-4 bg-[#78B2FB]" onClick={playNote6}></div>
+
+          {/* Certification logo - centered on mobile, right on desktop */}
+          <div className="flex justify-center order-1 md:order-2">
+            <img
+              src="/images/iso.png"
+              alt="ISO/IEC 27001 Information Security Management System Certification"
+              className="h-16 md:h-24 w-auto opacity-90 hover:opacity-100 transition-opacity duration-300 "
+            />
+          </div>
+
+          {/* Interactive sound notes */}
+          <div className="flex gap-0.5 order-3 cursor-pointer">
             <div
-              className="w-3 h-4 bg-black dark:bg-white"
+              className="w-3 h-4 bg-[#33DC72] hover:opacity-80 transition-opacity"
+              onClick={playNote1}
+            ></div>
+            <div
+              className="w-3 h-4 bg-[#DEB887] hover:opacity-80 transition-opacity"
+              onClick={playNote2}
+            ></div>
+            <div
+              className="w-3 h-4 bg-[#F98B8B] hover:opacity-80 transition-opacity"
+              onClick={playNote3}
+            ></div>
+            <div
+              className="w-3 h-4 bg-[#9EA7FA] hover:opacity-80 transition-opacity"
+              onClick={playNote4}
+            ></div>
+            <div
+              className="w-3 h-4 bg-[#6BCFFA] hover:opacity-80 transition-opacity"
+              onClick={playNote5}
+            ></div>
+            <div
+              className="w-3 h-4 bg-[#78B2FB] hover:opacity-80 transition-opacity"
+              onClick={playNote6}
+            ></div>
+            <div
+              className="w-3 h-4 bg-black dark:bg-white hover:opacity-80 transition-opacity"
               onClick={playNote7}
             ></div>
           </div>
